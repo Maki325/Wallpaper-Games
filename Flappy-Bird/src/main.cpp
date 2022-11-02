cb29bb7780c222dc1e537548e40611b94de3c601
Maki325 <markoni521@gmail.com>
2022-11-02 23:09:10 +0100

    Added imgui Needs some fixing

--- Flappy-Bird/src/main.cpp
+++ Flappy-Bird/src/main.cpp
@@ -1,12 +1,100 @@
 #include "pch.h"
 #include "Core/Application.h"
 #include "Utils/Utils.h"
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+#include <imgui.h>
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+void errorfun(int code, const char* message)
+{
+  std::cout << "Code: " << code << std::endl;
+  std::cout << "Message: " << message << std::endl;
+
+  std::cin.get();
+}
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR pCmdLine, _In_ int nCmdShow) {
   WallpaperAPI::Utils::InitConsole();
+
+  glfwSetErrorCallback(errorfun);
+
+  if (!glfwInit())
+    return 1;
+
+  const char* glsl_version = "#version 150";
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+
+  GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", NULL, NULL);
+  if (window == NULL)
+    return 1;
+
+
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(1); // Enable vsync
+
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+  // Setup Dear ImGui style
+  ImGui::StyleColorsDark();
+  //ImGui::StyleColorsClassic();
+
+
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init("#version 410");
+
+  bool show_demo_window = true;
+
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwPollEvents();
+
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::ShowDemoWindow(&show_demo_window);
+
+
+    // Rendering
+    ImGui::Render();
+    int display_w, display_h;
+    glfwGetFramebufferSize(window, &display_w, &display_h);
+    GL_CHECK(glViewport(0, 0, display_w, display_h));
+    GL_CHECK(glClearColor(0.2f, 0.3f, 0.9f, 1.0f));
+    GL_CHECK(glClear(GL_COLOR_BUFFER_BIT));
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    glfwSwapBuffers(window);
+  }
+  // ImGui::Begin("Window A");
+  // ImGui::Text("This is window A");
+  // ImGui::End();
   
-  WallpaperAPI::Application app;
-  app.Run();
+  // Cleanup
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  // WallpaperAPI::Application app;
+  // app.Run();
 
   return 0;
 }
