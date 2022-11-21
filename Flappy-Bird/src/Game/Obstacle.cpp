b2004e256c3814e438c6293f0e8e47cdcfcf1253
Maki325 <markoni521@gmail.com>
2022-11-22 00:03:10 +0100

    Extended the AABBs of the obstacles, so that the player can't jump over them Render the AABBs only if we are not in Distribution mode

--- Flappy-Bird/src/Game/Obstacle.cpp
+++ Flappy-Bird/src/Game/Obstacle.cpp
@@ -5,14 +5,14 @@
 namespace WallpaperAPI
 {
   const float offset = 1.1f;
-  const glm::vec2 hitboxSize(0.100f, 0.615f);
+  const glm::vec2 hitboxSize(0.100f, 5.615f);
 
   Obstacle::Obstacle()
     : m_position(0, 0, 0),
     m_top(Loader::LoadObj("resources/models/pipe.obj"), glm::vec3(0, offset, 0), glm::vec3(180, 0, 0), glm::vec3(0.25), glm::vec3(0), "resources/textures/pipe.png"),
     m_bottom(Loader::LoadObj("resources/models/pipe.obj"), glm::vec3(0, -offset, 0), glm::vec3(0, 0, 0), glm::vec3(0.25), glm::vec3(0), "resources/textures/pipe.png"),
-    m_topAABB(glm::vec2(0, offset), hitboxSize),
-    m_bottomAABB(glm::vec2(0, -offset), hitboxSize)
+    m_topAABB(glm::vec2(0, offset + 5), hitboxSize),
+    m_bottomAABB(glm::vec2(0, -offset - 5), hitboxSize)
   {
   }
 
@@ -21,8 +21,10 @@ namespace WallpaperAPI
     renderer.RenderEntity(m_top);
     renderer.RenderEntity(m_bottom);
 
-    renderer.RenderAABB(m_topAABB);
-    renderer.RenderAABB(m_bottomAABB);
+    #ifndef WG_DIST
+      renderer.RenderAABB(m_topAABB);
+      renderer.RenderAABB(m_bottomAABB);
+    #endif
   }
 
   void Obstacle::SetPosition(glm::vec3& position)
@@ -32,8 +34,8 @@ namespace WallpaperAPI
     m_bottom.m_position.y = position.y - offset;
 
     m_topAABB.m_position.x = m_bottomAABB.m_position.x = position.x;
-    m_topAABB.m_position.y = position.y + offset;
-    m_bottomAABB.m_position.y = position.y - offset;
+    m_topAABB.m_position.y = position.y + offset + 5;
+    m_bottomAABB.m_position.y = position.y - offset - 5;
 
     m_position = position;
   }
